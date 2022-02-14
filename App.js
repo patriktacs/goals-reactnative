2ed2a3a3e43341460802c1daddaf61f76009b53a
patriktacs <patriktacs.reg@gmail.com>
2022-02-14 21:07:22 +0100

    remove inline style attributes, add properties to stylesheet

--- App.js
+++ App.js
@@ -3,21 +3,32 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Goal"
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}
+          style={styles.input}
         />
         <Button title="ADD" />
       </View>
-      <View>
-
-      </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
